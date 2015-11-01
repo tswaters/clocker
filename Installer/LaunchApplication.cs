60b2d735e50c272ee40c44afb62f381ea1fa6b98
Tyler Waters <tyler.waters@gmail.com>
2015-11-01 13:41:07 -0800

    Changes to setup.msi
    - No longer generates dependency setup.exe (everyone has .net, honestly)
    - Adding x64 vs. x86 setup projects
    - All outputs are now going to .\bin\x64/x86\
    - Options to install shortcuts in startup/program files
    - Option to run clocker after setup has finished

---
+++ Installer/LaunchApplication.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Collections;
+
+namespace Installer
+{
+    [RunInstaller(true)]
+    public partial class LaunchApplication : System.Configuration.Install.Installer
+    {
+        public LaunchApplication()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+            if (Context.Parameters["Run"] == "1")
+            {
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Directory.SetCurrentDirectory(path);
+                Process.Start(path + "\\Clocker.exe");
+            }
+        }
+
+    }
+
+
+}
